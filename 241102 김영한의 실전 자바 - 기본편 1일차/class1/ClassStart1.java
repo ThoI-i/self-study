10942cf5d153920f678fa5bbc2bc218777b79288
Theo <ojw1912@gmail.com>
2024-11-02 18:46:55 +0900

    241102 김영한의 실전 자바 - 기본편 1일차

---
+++ 241102 김영한의 실전 자바 - 기본편 1일차/class1/ClassStart1.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int stduent1age = 15;
+        int stduent1Grade = 90;
+
+        String student2Name = "학생2";
+        int stduent2age = 16;
+        int stduent2Grade = 80;
+
+        System.out.println("이름: " + student1Name + "나이: " + stduent1age + "성적: " + stduent1Grade);
+        System.out.println("이름: " + student2Name + "나이: " + stduent2age + "성적: " + stduent2Grade);
+    }
+}
